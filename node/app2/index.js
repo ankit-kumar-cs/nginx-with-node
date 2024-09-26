885b54fdd861326dd5bfc9aff39b1e59fe2cbac9
Ankit Kumar <ankit.kumar@financialforce.com>
2024-09-26 16:59:35 +0530

    CLX

--- node/app2/index.js
+++ node/app2/index.js
@@ -9,5 +9,3 @@ app.get('/app2/hello', (req, res) => {
   res.send('Hello route from App2')
 })
 app.listen(portNumber, () => console.log(`App2 Server is up and running on port number: ${portNumber}`));
-
-console.log('Server up successful');
